b12d016b6c049e208d6326f68db2236a9335e016
Rafael <73045285+RafaelGonzalez06@users.noreply.github.com>
2022-11-28 18:47:08 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // alert("Bienvenido a Mi Pagina") 
 
 function cartel() {
-    alert("Mi nombre es Rafael González. Soy de Iguazú - Misiones")
+    alert("Mi nombre es Rafael González. \n\nSoy de Iguazú - Misiones. \n\nEmail: rafag2023@gmail.com")
 }
 
 
@@ -20,4 +20,46 @@ let processScroll = () => {
   }
   document.addEventListener('scroll', processScroll);
 
-  // termina el proceso de la barra
+// termina el proceso de la barra
+
+
+// Configuracion boton modo oscuro
+const btnSwitch = document.querySelector('#switch');
+const barraProgreso = document.querySelector('#barra_progreso');
+const btnUp = document.querySelector('#boton_up');
+// const cartas = document.querySelectorAll('.carta');   //en revision
+
+    btnSwitch.addEventListener('click', () => {
+        document.body.classList.toggle('dark'); //coloca una clase al body
+        
+        barraProgreso.classList.toggle('progreso');  //coloca una clase a la barra de progreso. Esto es para poder modificar el color en el modo oscuro
+        btnUp.classList.toggle('boton_up');
+        btnSwitch.classList.toggle('active');
+
+        //en revision
+        // cartas.forEach(function(){
+        //     cartas.classList.toggle('carta_oscura');
+        // });
+
+    });
+
+
+
+// Configurar el boton de volver arriba
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 100){
+        document.querySelector('.go-top-container')
+        .classList.add('show');
+    }else{
+        document.querySelector('.go-top-container')
+        .classList.remove('show');
+    }
+}
+
+document.querySelector('.go-top-container')
+.addEventListener('click', () =>{
+    window.scrollTo({
+        top:0,
+        behavior: 'smooth'
+    });
+});
